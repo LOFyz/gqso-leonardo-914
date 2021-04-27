cb29e7bbf34fd946ffea5a4c5d227c4d1f722cb5
LOFyz <leonardo_farias2010@hotmail.com>
2021-04-26 22:44:17 -0300

    Feat: Declaração de funções e testes.

--- src/main/java/br/edu/ifal/gqso/bankAccountCore.java
+++ src/main/java/br/edu/ifal/gqso/bankAccountCore.java
@@ -1,5 +1,18 @@
 package br.edu.ifal.gqso;
 
 public class bankAccountCore {
+  double balance;
+
+  void deposit (double value){
+    
+  }
+  
+  void draft (double value){
+
+  }
+  
+  double checkBalance (){
+    return balance;
+  }
   
 }
--- src/test/java/br/edu/ifal/gqso/bankAccountCoreTest.java
+++ src/test/java/br/edu/ifal/gqso/bankAccountCoreTest.java
@@ -1,5 +1,13 @@
 package br.edu.ifal.gqso;
 
+import org.junit.jupiter.api.Test;
+
 public class bankAccountCoreTest {
+  @Test
+  void testDepositIsValid (double value) {
+  }
   
+  @Test
+  void testDraftIsValid (double value) {
+  }
 }
